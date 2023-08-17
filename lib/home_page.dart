72e472721f1db611e495e0444047416119c7c7fb
Omkar Bhalbar <omkarbhalbar22@gmail.com>
2023-08-17 13:08:22 +0530

    Day1 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  // const HomePage({super.key});
+  final int days = 1;
+  final name = "Omkar";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Catolog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Hello $name, This $days at the Flutter"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -125,8 +125,9 @@ class _MyHomePageState extends State<MyHomePage> {
 }*/
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home_page.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
@@ -135,14 +136,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home:Material(
-       child: Center(
-        child:Container(
-         child: Text("Hello World"),
-         ),
-        ), 
-      ),
-    );
+        return MaterialApp(
+          home: HomePage(),
+        );
   }
 }
