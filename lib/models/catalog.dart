5bb8d4832a77c1a545b426bf556de573e9799656
Omkar Bhalbar <omkarbhalbar22@gmail.com>
2023-08-27 11:53:03 +0530

    day7 Completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,12 +1,35 @@
-class Item {
-  final String id;
-  final String name; 
-  final String desc; 
-  final num price; 
-  final String color; 
-  final String image;
 
-  Item(this.id, this.name, this.desc, this.price, this.color, this.image); 
-  
+
+class Item {
+  final int id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String  image;
+
+  Item(this.id,this.name, this.desc, this.price, this.color, this.image);
+
   
 }
+class  CatalogModel{
+  static final items=[
+
+    Item(1,"iPhone 12 Pro","Apple iPhone 12th generation",999,"#33505a","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc")
+  ];
+
+}
+// class CatalogModel {
+//   static final items = [
+//     Item(
+//         id:1,
+//         name: "iPhone 12 Pro",
+//         desc: "Apple iPhone 12th generation",
+//         price: 999,
+//         color: "#33505a",
+//         image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc"
+        
+//    )
+   
+//   ];
+// }
---
+++ lib/utiles/widgets/item_widgets.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/catalog.dart';
+
+class ItemWidgets extends StatelessWidget {
+  final Item item;
+
+  const ItemWidgets({super.key, required this.item}) : assert(item != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: () {
+          print("${item.name} pressed");
+        },
+        leading: Image.network(
+          item.image,
+          width: 100,
+        ),
+        title: Text(item.name),
+        subtitle: Text(item.desc),
+        trailing: Text(
+          "\$${item.price}",
+          textScaleFactor: 1.5,
+          style: TextStyle(
+            color: Colors.deepPurple,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
